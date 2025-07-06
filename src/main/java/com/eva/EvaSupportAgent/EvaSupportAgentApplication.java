281cbc5d5f81c25ff640b36b36a85eff7fad1dde
wesleee1 <shanpieo01@gmail.com>
2025-07-06 15:53:13 +0530

    commit

---
+++ src/main/java/com/eva/EvaSupportAgent/EvaSupportAgentApplication.java
@@ -0,0 +1,13 @@
+package com.eva.EvaSupportAgent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaSupportAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvaSupportAgentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eva/EvaSupportAgent/EvaSupportAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eva.EvaSupportAgent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvaSupportAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
